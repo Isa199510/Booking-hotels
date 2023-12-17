fc05ea943871a5cd4eb6d56c03547efb119a8fe7
Isa <isa.arbuhov@mail.ru>
2023-12-17 09:14:59 +0300

    Initial Commit

---
+++ Booking hotels/Booking_hotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booking_hotelsApp.swift
+//  Booking hotels
+//
+//  Created by Иса on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Booking_hotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
